adb5aa490415237d035e47c0f319eac522b3b348
Henrique Rodrigues da Silva <henrique@smartinnovation.com.br>
2021-10-22 17:40:34 -0300

    Commit Inicial

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import { VscGithubInverted } from 'react-icons/vsc'
+import { AuthContext } from '../../contexts/auth';
+import styles from './styles.module.scss';
+
+export function LoginBox() {
+    const { signInURL } = useContext(AuthContext);
+    
+    return (
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe sua mensagem</strong>
+            <a href={signInURL} className={styles.signInWithGithub}>
+                <VscGithubInverted size="24" />
+                Entrar com o GitHub
+            </a>
+        </div>
+    )
+}
